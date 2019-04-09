a9950f54eaf610223896a187f25146e1724f9657
hlam0779 <41835395+hlam0779@users.noreply.github.com>
2019-04-09 18:22:26 +0800

    Update telegram_update.py

--- telegram_update.py
+++ telegram_update.py
@@ -139,7 +139,7 @@ def crawling_dengue_data(url='https://data.gov.sg/dataset/e7536645-6126-4358-b95
 
 MessageLoop(bot, {'chat':handle}).run_as_thread()
 
-schedule.every().monday.do(update)
+schedule.every(10).seconds.do(update)
 schedule.every().day.do(crawling_dengue_data)
 
 css = open('CSS.txt', 'r').read()
@@ -177,7 +177,7 @@ while True:
     text_file.close()
     imgkit.from_file("haze_status.html", "haze_status.png", options=imgkitoptions)
     schedule.run_pending()
-    time.sleep(43200)
+    time.sleep(10)
 
 
 
